b541146241ca2ca3ac81ab6fce1fe61114a21a4e
alvarigno <alvaro.emparan@gmail.com>
2016-08-16 18:15:07 -0300

    actualizar

---
+++ WebAppCalendar/WebAppCalendar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppCalendar.MvcApplication" Language="C#" %>
---
+++ WebAppCalendar/WebAppCalendar/Models/EmpModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCalendar.Models
+{
+    public class EmpModel
+    {
+        [Display(Name = "Enter Date")]
+        public DateTime EnterDate { get; set; }
+    }
+}
