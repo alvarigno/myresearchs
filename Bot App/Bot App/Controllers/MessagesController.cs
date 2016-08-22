3275f721d4e753301fbc5a2df04d3ce744cdb498
alvarigno <alvaro.emparan@gmail.com>
2016-08-22 18:44:55 -0300

    update Script Key

--- Bot App/Bot App/Controllers/MessagesController.cs
+++ Bot App/Bot App/Controllers/MessagesController.cs
@@ -68,54 +68,6 @@ namespace Bot_App
             return null;
         }
 
-        public async Task<Message> Post([FromBody]Message message)
-        {
-            if (message.Type == "Message")
-            {
-                // calculate something for us to return  
-                int length = (message.Text ?? string.Empty).Length;
-
-                // return our reply to the user  
-                return message.CreateReplyMessage($"You sent {length} characters");
-            }
-            else
-            {
-                return HandleSystemMessage(message);
-            }
-        }
-
-        private Message HandleSystemMessage(Message message)
-        {
-            if (message.Type == "Ping")
-            {
-                Message reply = message.CreateReplyMessage();
-                reply.Type = "Ping";
-                return reply;
-            }
-            else if (message.Type == "DeleteUserData")
-            {
-                // Implement user deletion here  
-                // If we handle user deletion, return a real message  
-            }
-            else if (message.Type == "BotAddedToConversation")
-            {
-            }
-            else if (message.Type == "BotRemovedFromConversation")
-            {
-            }
-            else if (message.Type == "UserAddedToConversation")
-            {
-            }
-            else if (message.Type == "UserRemovedFromConversation")
-            {
-            }
-            else if (message.Type == "EndOfConversation")
-            {
-            }
-
-            return null;
-        }
-
     }
 
 }
