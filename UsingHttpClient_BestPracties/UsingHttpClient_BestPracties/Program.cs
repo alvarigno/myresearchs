e3212b8859655917e9b3a0a35de4339d55e4776d
alvarigno <alvaro.emparan@gmail.com>
2016-09-05 10:19:24 -0300

    Add Comment

---
+++ UsingHttpClient_BestPracties/UsingHttpClient_BestPracties/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingHttpClient_BestPracties
+{
+    class Program
+    {
+        private static HttpClient Client = new HttpClient();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Starting connections");
+            for (int i = 0; i < 10; i++)
+            {
+                var result = Client.GetAsync("http://www.alvaroemparan.cl").Result;
+                Console.WriteLine(result.StatusCode);
+            }
+            Console.WriteLine("Connections done");
+            Console.ReadLine();
+
+        }
+    }
+}
