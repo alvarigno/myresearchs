0c0ee5d963be1c2b4ba0944a7b7a2b5caa60e2b0
alvarigno <alvaro.emparan@gmail.com>
2017-02-14 10:38:43 -0300

    actualización

--- webpay_TBK/Transbank.NET/myConnection.cs
+++ webpay_TBK/Transbank.NET/myConnection.cs
@@ -11,12 +11,14 @@ namespace Transbank.NET
 
         public static SqlConnection GetConnection()
         {
-            string str = "data source=172.16.0.241;initial catalog=baseprod2;user id=usrBDDesa;password=*pwdBD*;";
-            //string str = "Data Source=10.0.0.158; Initial Catalog=bdmailparser;User ID=usdes; Password=Su_4320$.x";
+
+            string str = "data source=10.0.0.158; initial catalog=desarrollo;Persist Security Info=True; user id=usdes;password=Su_4320$.x;";//Desarrollo
+            //string str = "data source=172.16.0.241;initial catalog=baseprod2;user id=usrBDDesa;password=*pwdBD*;";//Base local
             SqlConnection con = new SqlConnection(str);
             con.Open();
             return con;
         }
 
     }
+
 }
