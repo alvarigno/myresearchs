6d5bc1ee59f1a002b825f5b437299ab4dd9db762
alvarigno <alvaro.emparan@gmail.com>
2017-05-30 11:59:17 -0400

    add new project to research other way to do that

---
+++ publicaCA-DM/publicaCA-DM/frmDM.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNetBrowser;
+using DotNetBrowser.DOM;
+using DotNetBrowser.Events;
+using DotNetBrowser.WinForms;
+
+namespace publicaCA_DM
+{
+    public partial class frmDM : Form
+    {
+        public static Browser browserDM;
+       public frmDM()
+        {
+            InitializeComponent();
+
+            browserDM = BrowserFactory.Create();
+            BrowserView browserView = new WinFormsBrowserView(browserDM);
+            Controls.Add((Control)browserView);
+
+            ManualResetEvent waitEvent = new ManualResetEvent(false);
+            browserDM.FinishLoadingFrameEvent += delegate (object sender, FinishLoadingEventArgs e)
+            {
+                // Wait until main document of the web page is loaded completely.
+                if (e.IsMainFrame)
+                {
+                    waitEvent.Set();
+                }
+            };
+
+            browserDM.LoadURL("http://autos.demotores.cl/");
+            waitEvent.WaitOne();
+
+            browserDM.ExecuteJavaScript("showLoginModal();");
+
+            browserDM.ExecuteJavaScript("document.getElementById('email').value = 'iduprueba@dm.cl'");
+            browserDM.ExecuteJavaScript("document.getElementById('Password').value = 'demotores2017'");
+
+            var strJavascript = "var nouEvent = document.createEvent('MouseEvents');";
+            strJavascript +=
+                "nouEvent.initMouseEvent('click', true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+            strJavascript += "var objecte = document.getElementById('enterAjaxLogin');";
+            strJavascript += "var canceled = !objecte.dispatchEvent(nouEvent);";
+            browserDM.ExecuteJavaScript(strJavascript);
+            //waitEvent.WaitOne();
+
+            strJavascript = "var nouEvent = document.createEvent('MouseEvents');";
+            strJavascript +=
+                "nouEvent.initMouseEvent('click', true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+            strJavascript += "var objecte = document.getElementById('sellLink');";
+            strJavascript += "var canceled = !objecte.dispatchEvent(nouEvent);";
+            browserDM.ExecuteJavaScript(strJavascript);
+            //waitEvent.WaitOne();
+
+            strJavascript = "var nouEvent = document.createEvent('MouseEvents');";
+            strJavascript +=
+                "nouEvent.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+            strJavascript += "var objecte = document.getElementById('selectVehicleType');";
+            strJavascript += "var canceled = !objecte.dispatchEvent(nouEvent);";
+            browserDM.ExecuteJavaScript(strJavascript);
+            
+
+
+
+
+
+
+
+            //browser.ExecuteJavaScript("document.getElementById('moto').checked = true;");
+            //
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ publicaCA-DM/publicaCA-DM/frmPrincipal.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace publicaCA_DM
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            Size ResolucionPantalla = System.Windows.Forms.SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            string mensaje = "alto  " + ResolucionPantalla.Height + " ancho " + ResolucionPantalla.Width;
+            int altoForm = ResolucionPantalla.Height - 50;
+            int anchoForm = (ResolucionPantalla.Width/2) - 50;
+
+            frmCA formCA = new frmCA
+            {
+                MdiParent = this,
+                Height = altoForm,
+                Width = anchoForm
+            };
+            formCA.Show();
+
+            //MessageBox.Show(mensaje, "Resolución pantalla");
+            frmDM frm = new frmDM
+            {
+                MdiParent = this,
+                Height = altoForm,
+                Width = anchoForm,
+                Top = 1,
+                Left = (ResolucionPantalla.Width / 2) 
+                
+            };
+            frm.Show();
+
+            
+            //MessageBox(frmPrincipal.)
+        }
+
+        private void chileautosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Size ResolucionPantalla = System.Windows.Forms.SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            string mensaje = "alto  " + ResolucionPantalla.Height + " ancho " + ResolucionPantalla.Width;
+            int altoForm = ResolucionPantalla.Height - 50;
+            int anchoForm = (ResolucionPantalla.Width / 2) - 50;
+
+            frmCA formCA = new frmCA
+            {
+                MdiParent = this,
+                Height = altoForm,
+                Width = anchoForm
+            };
+            formCA.Show();
+        }
+
+        private void demotoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Size ResolucionPantalla = System.Windows.Forms.SystemInformation.PrimaryMonitorMaximizedWindowSize;
+            string mensaje = "alto  " + ResolucionPantalla.Height + " ancho " + ResolucionPantalla.Width;
+            int altoForm = ResolucionPantalla.Height - 50;
+            int anchoForm = (ResolucionPantalla.Width / 2) - 50;
+
+            frmDM frm = new frmDM
+            {
+                MdiParent = this,
+                Height = altoForm,
+                Width = anchoForm,
+                Top = 1,
+                Left = (ResolucionPantalla.Width / 2)
+
+            };
+            frm.Show();
+        }
+
+        private void patenteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((frmCA.browserCA.URL.ToString() ==
+                "http://desarrollofotos.chileautos.cl/actualizadores/paginas/chileautos/opciones.asp") && (frmDM.browserDM.URL.ToString() == "http://www.demotores.cl/frontend/publicacion.html?execution=e1s2"))
+            {
+                string strPatente = frmCA.browserCA.ExecuteJavaScriptAndReturnValue("document.getElementById('patente').value").ToString();
+                frmDM.browserDM.ExecuteJavaScript("document.getElementById('licensePlate').value='" + strPatente + "'");
+            }
+            MessageBox.Show(frmCA.browserCA.URL.ToString());
+        }
+    }
+}
