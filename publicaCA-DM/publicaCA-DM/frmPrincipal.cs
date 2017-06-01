9eca138a5114efb5e7d4475c715963646aa6c7d7
alvarigno <alvaro.emparan@gmail.com>
2017-06-01 17:14:08 -0400

    actualización de SCRIPT de RESPALDO

--- publicaCA-DM/publicaCA-DM/frmPrincipal.cs
+++ publicaCA-DM/publicaCA-DM/frmPrincipal.cs
@@ -108,6 +108,7 @@ namespace publicaCA_DM
                 string script = "(function() {return document.getElementById('patente').value;})();";
                 string marca = "(function(){ var e = document.getElementById('cod_marca');return e.options[e.selectedIndex].text;})();";
                 string modelo = "(function(){ var e = document.getElementById('modelo');return e.options[e.selectedIndex].text;})();";
+                string version = "(function(){ var e = document.getElementById('version');return e.options[e.selectedIndex].text;})();";
 
 
                 var task = frmCA.chromeBrowser.EvaluateScriptAsync(script);
@@ -181,11 +182,11 @@ namespace publicaCA_DM
                             //frmDM.chromeBrowser2.ExecuteScriptAsync("document.getElementById('models').onchange();", TimeSpan.FromSeconds(1));
 
                             // Lanzo el evento onchange de la marca en demotores para que se carguen las versiones
-                            string strJavascript = "var nouEvent = document.createEvent('MouseEvents');";
-                            strJavascript += "nouEvent.initMouseEvent('change', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
-                            strJavascript += "var objecte = document.getElementById('models');";
-                            strJavascript += "var canceled = !objecte.dispatchEvent(nouEvent);";
-                            frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript);
+                            //string strJavascript = "var nouEvent = document.createEvent('MouseEvents');";
+                            //strJavascript += "nouEvent.initMouseEvent('change', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+                            //strJavascript += "var objecte = document.getElementById('models');";
+                            //strJavascript += "var canceled = !objecte.dispatchEvent(nouEvent);";
+                            //frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript);
 
                             //strModelo = response.Result.ToString();
                             //frmDM.chromeBrowser2.ExecuteScriptAsync("var textToFind = '" + strModelo + "';var dd = document.getElementById('models');for (var i = 0; i < dd.options.length; i++){if (dd.options[i].text === textToFind){dd.selectedIndex = i;dd.options[i].setAttribute('selected', 'selected');break;}}");
@@ -193,11 +194,11 @@ namespace publicaCA_DM
 
 
                             // Lanzo el evento onchange de la marca en demotores para que se carguen los modelos
-                            //string strJavascript2 = "var nouEvent = document.createEvent('MouseEvents');";
-                            //strJavascript2 += "nouEvent.initMouseEvent('change', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
-                            //strJavascript2 += "var objecte = document.getElementById('models');";
-                            //strJavascript2 += "var canceled = !objecte.dispatchEvent(nouEvent);";
-                            //frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript2);
+                            string strJavascript3 = "var nouEvent = document.createEvent('MouseEvents');";
+                            strJavascript3 += "nouEvent.initMouseEvent('change', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+                            strJavascript3 += "var objecte = document.getElementById('models');";
+                            strJavascript3 += "var canceled = !objecte.dispatchEvent(nouEvent);";
+                            frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript3);
 
 
 
@@ -208,7 +209,41 @@ namespace publicaCA_DM
                 });
 
 
-               
+                var taskversion = frmCA.chromeBrowser.EvaluateScriptAsync(version);
+                taskversion.ContinueWith(v =>
+                {
+                    if (!v.IsFaulted)
+                    {
+                        var response3 = v.Result;
+                        if (response3.Success && response3.Result != null)
+                        {
+
+                            string strVersion = response3.Result.ToString();
+                            strVersion = strVersion.ToLower();
+                            strVersion = strVersion.First().ToString().ToUpper() + strVersion.Substring(1);
+                            //selecciono el modelo en demotores
+                            //MessageBox.Show("cargando Modelos");
+                            int milliseconds = 4000;
+                            Thread.Sleep(milliseconds);
+                            string strJavascript2 = "var x = document.getElementById('versions').options.length;alert('version'+x);<function cargamodelo(){ var modeloEncontrado = false; var datomodelo = '" + strVersion + "'; var textToFind2 = datomodelo;var dd = document.getElementById('versions'); for (var i = 0; i < dd.options.length; i++){if (dd.options[i].text === textToFind2){dd.selectedIndex = i;dd.options[i].setAttribute('selected', 'selected');break;}}}setTimeout('cargamodelo()',1000);";
+
+                            frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript2);
+
+                            // Lanzo el evento onchange de la marca en demotores para que se carguen los modelos
+                            string strJavascript3 = "var nouEvent = document.createEvent('MouseEvents');";
+                            strJavascript3 += "nouEvent.initMouseEvent('change', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);";
+                            strJavascript3 += "var objecte = document.getElementById('versions');";
+                            strJavascript3 += "var canceled = !objecte.dispatchEvent(nouEvent);";
+                            frmDM.chromeBrowser2.ExecuteScriptAsync(strJavascript3);
+
+
+
+                        }
+                        // MessageBox.Show(strModelo + "-" + urlbr1 + "-" + urlbr2);
+                    }
+                    taskmodelo.Dispose();
+                });
+
 
             }
 
