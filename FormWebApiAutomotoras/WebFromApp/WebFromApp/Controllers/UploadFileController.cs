dd9d31200b77c50309a1a2487989cb1e85ac64ac
alvarigno <alvaro.emparan@gmail.com>
2017-06-29 18:39:16 -0400

    Actualización de script

---
+++ FormWebApiAutomotoras/WebFromApp/WebFromApp/Controllers/UploadFileController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFromApp.Controllers
+{
+    public class UploadFileController : Controller
+    {
+
+        public ActionResult Upload() {
+
+
+            return View();
+
+        }
+
+    }
+}
