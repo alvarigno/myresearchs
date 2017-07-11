b251c9ee82e4ba5730d19f3b950303db23748f6c
alvarigno <alvaro.emparan@gmail.com>
2017-07-11 15:00:08 -0400

    Actualización de Script de optención de ip de cliente

--- FormWebApiAutomotoras/WebFromApp/WebFromApp/Controllers/UploadFileController.cs
+++ FormWebApiAutomotoras/WebFromApp/WebFromApp/Controllers/UploadFileController.cs
@@ -11,10 +11,32 @@ namespace WebFromApp.Controllers
 
         public ActionResult Upload() {
 
-
+            ViewBag.ip =  GetIPAddress();
             return View();
 
         }
+
+        /// <summary>
+        ///  Get ip address from url client.
+        /// </summary>
+        /// <returns></returns>
+        protected string GetIPAddress()
+        {
+            System.Web.HttpContext context = System.Web.HttpContext.Current;
+            string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (!string.IsNullOrEmpty(ipAddress))
+            {
+                string[] addresses = ipAddress.Split(',');
+                if (addresses.Length != 0)
+                {
+                    return addresses[0];
+                }
+            }
+
+            return context.Request.ServerVariables["REMOTE_ADDR"];
+        }
+
 
     }
 }
