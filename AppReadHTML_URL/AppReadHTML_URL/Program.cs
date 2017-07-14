33c95554a222827820c32a80f1a9b6b389804cc1
alvarigno <alvaro.emparan@gmail.com>
2017-07-14 16:31:57 -0400

    Actualización de script de codificación.

--- AppReadHTML_URL/AppReadHTML_URL/Program.cs
+++ AppReadHTML_URL/AppReadHTML_URL/Program.cs
@@ -11,24 +11,32 @@ namespace AppReadHTML_URL
     class Program
     {
 
-        static string filePath = "http://www.julioinfante.com/ficha/49162";
+        static string filePath = "http://www.julioinfante.com/ficha/";
         static void Main(string[] args)
         {
 
+            string codigo = Console.ReadLine();
+            Console.WriteLine(codigo);
             // From File
             var doc = new HtmlDocument();
 
             // From Web
-            var url = filePath;
+            var url = filePath+codigo;
             var web = new HtmlWeb();
             doc = web.Load(url);
 
             List<string> ListadoImagenes = ObtieneImagenes(doc);
             List<string> ListadoDatos = ObtieneDatos(doc);
+            List<string> Listadotitulo = ObtieneTitulo(doc);
+            List<string> Listadovalortachado = ObtienValorTachado(doc);
+            List<string> Listadovalorbono = ObtienValorBono(doc);
+           // List<string> Listadoprueba = pruebauno(doc); 
 
-
-            MuestraContenido(ListadoImagenes);
-            MuestraContenido(ListadoDatos);
+            MuestraContenido(ListadoImagenes, "imágenes");
+            MuestraContenido(ListadoDatos, "listado de datos");
+            MuestraContenido(Listadotitulo, "título de la publicación");
+            MuestraContenido(Listadovalorbono, "Valor bonificado");
+            MuestraContenido(Listadovalortachado, "Valor tachado");
 
             Console.ReadLine();
         }
@@ -49,30 +57,91 @@ namespace AppReadHTML_URL
         public static List<string> ObtieneDatos(HtmlDocument doc) {
 
             var localdoc = new HtmlDocument();
-            localdoc =  doc;
+            localdoc = doc;
             List<string> retorna = new List<string>();
-            
-            if (localdoc.DocumentNode.SelectNodes("//h1/span").First().Attributes["itemprop"].Value == "name") {
 
-                var query = localdoc.DocumentNode.SelectNodes("//h1/span");
-                foreach (var data in query)
-                {
+            var query = localdoc.DocumentNode.Descendants("span")
+            .Where(c => (c.Attributes["itemprop"] != null) && (c.Attributes["itemprop"].Value == "description"))
+            .SelectMany(c => c.Descendants());
 
-                    retorna.Add(data.InnerText);
 
-                }
+            foreach (var d in query) {
+
+                retorna.Add(d.InnerHtml);
+
             }
 
             return retorna;
 
         }
 
-        public static void MuestraContenido(List<string> data) {
+        public static List<string> ObtieneTitulo(HtmlDocument doc)
+        {
 
+            var localdoc = new HtmlDocument();
+            localdoc = doc;
+            List<string> retorna = new List<string>();
 
-            foreach (var img in data)
+            var query = localdoc.DocumentNode.Descendants("div")
+            .Where(c => (c.Attributes["class"] != null) && (c.Attributes["class"].Value == "row pagetitle"))
+            .SelectMany(c => c.DescendantsAndSelf("h1"));
+
+            foreach (var d in query)
             {
-                Console.WriteLine("Contenido: " + img);
+
+                retorna.Add(d.InnerText);
+
+            }
+
+            return retorna;
+        }
+
+        public static List<string> ObtienValorTachado(HtmlDocument doc)
+        {
+
+            var localdoc = new HtmlDocument();
+            localdoc = doc;
+            List<string> retorna = new List<string>();
+            var query = localdoc.DocumentNode.Descendants("div")
+            .Where(c => (c.Attributes["class"] != null) && (c.Attributes["class"].Value == "tags price tachado"))
+            .SelectMany(c => c.DescendantsAndSelf());
+
+            foreach (var d in query)
+            {
+
+                retorna.Add(d.InnerText);
+
+            }
+
+            return retorna;
+
+        }
+
+        public static List<string> ObtienValorBono(HtmlDocument doc)
+        {
+
+            var localdoc = new HtmlDocument();
+            localdoc = doc;
+            List<string> retorna = new List<string>();
+            var query = localdoc.DocumentNode.Descendants("div")
+            .Where(c => (c.Attributes["class"] != null) && (c.Attributes["class"].Value == "tags bono"))
+            .SelectMany(c => c.DescendantsAndSelf());
+            
+            foreach (var d in query)
+            {
+                retorna.Add(d.InnerText);
+            }
+
+            return retorna;
+            
+        }
+
+        public static void MuestraContenido(List<string> data, string seccion) {
+
+            Console.WriteLine("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Sección: "+ seccion);
+            foreach (var d in data)
+            {
+                Console.WriteLine("-> " + d);
             }
 
         }
