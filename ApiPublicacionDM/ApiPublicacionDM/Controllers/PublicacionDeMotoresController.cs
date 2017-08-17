e104642fd8c22e04dec9ae341280c09fca82ca85
alvarigno <alvaro.emparan@gmail.com>
2017-08-17 20:03:05 -0300

    Creación de la base de la api para obtener datos para publicar aviso en DeMotores

---
+++ ApiPublicacionDM/ApiPublicacionDM/Controllers/PublicacionDeMotoresController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiPublicacionDM.Controllers
+{
+    public class PublicacionDeMotoresController : ApiController
+    {
+
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> AdministraAvisoDm() {
+
+            HttpResponseMessage result = null;
+            var httpRequest = HttpContext.Current.Request;
+
+            if (httpRequest.Form.Count > 0 && httpRequest.Form.Keys[0] == "codautoCA")
+            {
+                if (httpRequest.Form["codautoCA"] == "12345")
+                {
+                    result = Request.CreateResponse(HttpStatusCode.OK, httpRequest.Form["codautoCA"]);
+                }
+                else {
+
+                    result = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Código no existe.");
+
+                }
+            }
+            else {
+
+                result = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Campo no existe.");
+            }
+            return result;
+        }
+
+
+    }
+}
