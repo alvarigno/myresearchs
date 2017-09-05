70ab331b1a49a73abf17dcd140df3c8aa3f13069
alvarigno <alvaro.emparan@gmail.com>
2017-09-05 16:53:29 -0300

    Actualización de script de carga de archivos XML.

--- FormWebApiAutomotoras/WebFromApp/WebFromApp/Controllers/UploadFileController.cs
+++ FormWebApiAutomotoras/WebFromApp/WebFromApp/Controllers/UploadFileController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Mvc;
 
@@ -37,6 +40,30 @@ namespace WebFromApp.Controllers
             return context.Request.ServerVariables["REMOTE_ADDR"];
         }
 
+
+        public static void SubeArchivos() {
+
+            HttpClient httpClient = new HttpClient();
+            MultipartFormDataContent form = new MultipartFormDataContent();
+
+            byte[] databytearraystring = dataFileToByteArray(@"C:\Chileautos_Desarrollo\desarrollo\integrador\fileLoaded\1028_0509201716234525.xml");
+            form.Add(new ByteArrayContent(databytearraystring, 0, databytearraystring.Count()), "1028_0509201716234525", "1028_0509201716234525.xml");
+            httpClient.DefaultRequestHeaders.Add("x-key", "B451AA1ACC6275A9DED6C799722B50CB");
+            HttpResponseMessage response = httpClient.PostAsync("http://desarrollo.chileautos.cl/integrador/API-CLAAutomotora/Upload", form).Result;
+
+            httpClient.Dispose();
+            string sd = response.Content.ReadAsStringAsync().Result;
+
+        }
+        
+        private static byte[] dataFileToByteArray(string fullFilePath)
+        {
+            FileStream fs = System.IO.File.OpenRead(fullFilePath);
+            byte[] bytes = new byte[fs.Length];
+            fs.Read(bytes, 0, Convert.ToInt32(fs.Length));
+            fs.Close();
+            return bytes;
+        }
 
     }
 }
