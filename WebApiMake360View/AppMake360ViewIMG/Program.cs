59f6c7d0a179e298fd9d4a232148b79f4ff9026d
alvarigno <alvaro.emparan@gmail.com>
2017-11-03 18:17:32 -0300

    Actualización de script de servicio y carga

--- WebApiMake360View/AppMake360ViewIMG/Program.cs
+++ WebApiMake360View/AppMake360ViewIMG/Program.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using UpLoadFile;
 
 namespace AppMake360ViewIMG
 {
@@ -33,62 +34,40 @@ namespace AppMake360ViewIMG
         public string ProcesaVista360(string filename, string placeapp) {
 
             string resultado = "";
-            
-            Process process = new Process();
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = false;
-            process.StartInfo.UseShellExecute = false;
-            process.Start();
-            process.StandardInput.WriteLine("cd "+placeapp);
-            process.StandardInput.WriteLine(@".\gear360pano.cmd " + filename);
-            process.StandardInput.Close();
-            process.WaitForExit();
 
-            string namefile = Path.GetFileNameWithoutExtension(filename);
+            while (resultado == "") { 
 
-            string curFile = placeapp + "data\\" + namefile + "_pano.jpg";
+                Process process = new Process();
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardInput = true;
+                process.StartInfo.RedirectStandardOutput = false;
+                process.StartInfo.UseShellExecute = false;
+                process.Start();
+                process.StandardInput.WriteLine("cd "+placeapp);
+                process.StandardInput.WriteLine(@".\gear360pano.cmd " + filename);
+                process.StandardInput.Close();
+                process.WaitForExit();
 
-            if (File.Exists(curFile))
-            {
+                string namefile = Path.GetFileNameWithoutExtension(filename);
 
-                Console.WriteLine("\nimagen 360 creada con éxito.");
-                resultado = UpLoadFile(curFile);
-                
+                string curFile = placeapp + "data\\" + namefile + "_pano.jpg";
+
+                if (File.Exists(curFile))
+                {
+                    Console.WriteLine("\nimagen 360 creada con éxito.");
+                    CA_ImgUploadServer subearchivo = new CA_ImgUploadServer();
+                    resultado = subearchivo.Uploadimage(curFile);
+                }
+
             }
-            
+
             return resultado;
 
         }
 
 
-        public static string UpLoadFile(string fileName) {
-
-            string resultado = "";
-
-            WebClient myWebClient = new WebClient();
-
-            byte[] responseArray = myWebClient.UploadFile("https://ws.chileautos.cl/api-cla/Upload/auto/particular", fileName);
-            
-            if (responseArray != null && responseArray.Length > 0)
-            {
-
-                String prova = "";
-
-                prova =  System.Text.Encoding.ASCII.GetString(responseArray);
-                
-                var test = JObject.Parse(prova);
-                JArray items = (JArray)test["images"];
-                string file = items.ToString().Replace("[","").Replace("]","").Replace("\n","").Replace("\r","").Replace(" ","").Replace("\"", "");
-                int length = items.Count;
-                Console.WriteLine("\nimagen a cargada en servidor con éxito. Url es: " + file);
-                resultado = file;
-
-            }
-
-            return resultado;
-        }
+       
 
     }
 }
---
+++ WebApiMake360View/UpLoadFile/CA_ImgUploadServer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+
+namespace UpLoadFile
+{
+    public class CA_ImgUploadServer
+    {
+
+        public const int OrientationId = 0x0112;
+
+        public string _urlPhotoServer = "https://chileautos.storage.csnstatic.com/chileautos/";
+        public string _urlPhotoServerFinal = "https://chileautos.pxcrush.net/chileautos/";
+
+        public byte[] imgToByteArray(string inImg)
+        {
+            MemoryStream ms = new MemoryStream();
+            Image img = Image.FromFile(inImg);
+
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            return ms.ToArray();
+
+        }
+
+        public string Uploadimage(String files)
+        {
+            int count = 1;
+            string categoria = "360";
+            string vendedor = "interior";
+
+            string fotos = "";
+
+            try
+            {
+
+
+                string urlUploadPhotoServer = _urlPhotoServer;
+                urlUploadPhotoServer = String.Concat(urlUploadPhotoServer, categoria, "/", vendedor, "/");
+                _urlPhotoServerFinal = String.Concat(_urlPhotoServerFinal, categoria, "/", vendedor, "/");
+
+
+                byte[] image = imgToByteArray(files);
+
+                if (count <= 20)
+                {
+                    using (var client = new HttpClient())
+                    {
+                        //client.DefaultRequestHeaders.TryAddWithoutValidation("Username", "liveimages-stg@chileautos.cl");
+                        //client.DefaultRequestHeaders.TryAddWithoutValidation("Password", "UdDX6eTJejSYBvTm7CMl");
+
+                        client.DefaultRequestHeaders.TryAddWithoutValidation("Username", "liveimages@chileautos.cl");
+                        client.DefaultRequestHeaders.TryAddWithoutValidation("Password", "13Hq0hMLVYweN4bNJVVf");
+
+
+                        Uri uri = new Uri(files);
+                        string filename = System.IO.Path.GetFileName(uri.LocalPath);
+
+                        var content = new MultipartFormDataContent();
+                        var imageContent = new ByteArrayContent(image);
+
+                        content.Add(imageContent, Path.GetFileName(files), filename);
+                        imageContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpeg");
+
+                        using (var response = client.PostAsync(urlUploadPhotoServer +"360_"+ DateTime.Now.ToFileTime() + ".jpg", content).Result)
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                var csnFile = response.Content.ReadAsStringAsync().Result;
+                                fotos = string.Concat(_urlPhotoServerFinal, csnFile);
+                            }
+                        }
+
+                    }
+                }
+                count = count + 1;
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("error: " + e.Message);
+
+            }
+
+            return fotos;
+        }
+
+    }
+}
