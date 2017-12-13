544b9f43b9db02ad7d26aebdb4678f65b4c6ee0a
alvarigno <alvaro.emparan@gmail.com>
2017-12-13 15:26:13 -0300

    Actualización de ejemplo de ventana de procesos.

---
+++ WpfAppProcessWindow/WpfAppProcessWindow/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppProcessWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        VMmain VM;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            VM = new VMmain();
+            this.DataContext = VM; //asignacion del viewmodel
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VM.PantallaActiva = false;
+            VM.VerMensaje = true;
+            await haceAlgo();
+            VM.VerMensaje = false;
+            VM.PantallaActiva = true;
+
+        }
+
+        internal async Task HaceUnProcesoPesado()
+        {
+
+            await haceAlgo();
+
+        }
+
+        internal async Task haceAlgo()
+        {
+            await Task.Run(() => System.Threading.Thread.Sleep(5000));
+        }
+
+
+    }
+
+}
---
+++ WpfAppProcessWindow/WpfAppProcessWindow/VMmain.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices; 
+
+namespace WpfAppProcessWindow
+{
+    class VMmain : INotifyPropertyChanged
+    {
+        protected void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public VMmain()
+        {
+
+            PantallaActiva = true;
+        }
+
+        private bool _PantallaActiva;
+
+        public bool PantallaActiva
+        {
+            get { return _PantallaActiva; }
+            set { _PantallaActiva = value; RaisePropertyChanged(); }
+        }
+
+
+        private bool _VerMensaje;
+        private string _Vermensajetext;
+
+        public bool VerMensaje
+        {
+            get { return _VerMensaje; }
+            set { _VerMensaje = value; RaisePropertyChanged(); }
+
+        }
+
+    }
+}
